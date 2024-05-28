d211e597d861f4d3e4ab4b46b80fae698d9d18c9
Malin M <malinmayalal@gmail.com>
2024-05-28 10:28:00 +0530

    import module and text file

---
+++ python_file2.py
@@ -0,0 +1,19 @@
+import python_file1
+import math
+import random
+print(python_file1.add(23,23))
+print(python_file1.sub(9,6))
+print(python_file1.multi(3,2))
+x=min(2,3,4,5,56,78,89,1,0)
+print(x)
+y=max(20,200,30,49,201,201.55)
+print(y)
+a=abs(-21);print(a)                             #To convert negative to positive
+b=pow(2,3);print(b)                             #To get the power of a value
+c=math.sqrt(225);print(c)                       #To get the squareroot
+d=math.pi;print(d)                              # Pi
+e=math.factorial(5);print(e)                    #Factorial 
+f=math.sin(45);print(f)                         #Sin(45)
+g=math.cos(45);print(g)                         #Cos(45)
+h=math.tan(45);print(h)                         #Tan(45)
+print(random.random())                          #To get a random no b/w 0 to 1
